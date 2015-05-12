ddf61c5eb629721731f56ab20041bdd24c027ba4
Yeison Arias <yeison.arias@umb.edu.co>
2015-05-12 10:27:04 -0500

    nuevos cambios elaboración de cuestionarios
    nuevos cambios elaboración de cuestionarios

--- vistas/elaboracion_de_cuestionarios/menu.php
+++ vistas/elaboracion_de_cuestionarios/menu.php
@@ -9,6 +9,33 @@
         <li <?php echo ($page['file'] == 'nuevo.html') ? 'class="active"' : ''; ?>>
             <a href="index.php?folder=elaboracion_de_cuestionarios&page=nuevo">Nuevo Cuestionario</a>
         </li>
+        <li <?php echo ($page['file'] == 'editar.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=editar">Editar Cuestionario</a>
+        </li>
+        <li <?php echo ($page['file'] == 'eliminar.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=eliminar">Eliminar Cuestionario</a>
+        </li>
+        <li <?php echo ($page['file'] == 'vista.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=vista">Vista Previa</a>
+        </li>
+        <li <?php echo ($page['file'] == 'horas.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=horas">Definir Horas</a>
+        </li>
+        <li <?php echo ($page['file'] == 'cargar.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=cargar">Cargar Preguntas</a>
+        </li>
+        <li <?php echo ($page['file'] == 'publicar.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=publicar">Publicar el Cuestionario</a>
+        </li>
+        <li <?php echo ($page['file'] == 'habilitar.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=habilitar">Habilitar Estudiantes</a>
+        </li>
+        <li <?php echo ($page['file'] == 'examen.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=examen">Visualizar examen realizado</a>
+        </li>
+        <li <?php echo ($page['file'] == 'replicar.html') ? 'class="active"' : ''; ?>>
+            <a href="index.php?folder=elaboracion_de_cuestionarios&page=replicar">Replicar cuestionario</a>
+        </li>
     </ul>
     <ul class="nav nav-sidebar">
     </ul>
